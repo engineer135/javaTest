cd08edcf6e2a999a0a1e3df8762b61aa0014005a
engineer135 <everafterk@naver.com>
2016-06-28 16:42:45 +0900

    Synchronized 적용

--- src/multiThread/sync/Calculator.java
+++ src/multiThread/sync/Calculator.java
@@ -7,7 +7,8 @@ public class Calculator {
 		return memory;
 	}
 
-	public void setMemory(int memory) {
+	// 동기화 메소드로 수정하거나...
+	/*public synchronized void setMemory(int memory) {
 		this.memory = memory;
 		
 		try {
@@ -16,5 +17,18 @@ public class Calculator {
 			e.printStackTrace();
 		}
 		System.out.println(Thread.currentThread().getName() + ": " + this.memory);
+	}*/
+	
+	public void setMemory(int memory) {
+		synchronized(this){
+			this.memory = memory;
+			
+			try {
+				Thread.sleep(2000); // 2초간 일시 정지
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			System.out.println(Thread.currentThread().getName() + ": " + this.memory);
+		}
 	}
 }
